7be478d96063fa9d1dc2f64190276f22d616ff5b
Shwuup <e_kong98z@hotmail.com>
2018-05-11 00:40:57 +1000

    Started working on the trie

---
+++ FreqGetter/src/Node.java
@@ -0,0 +1,5 @@
+
+public class Node {
+	String item;
+	String jap;
+}
--- FreqGetter/src/Test.java
+++ FreqGetter/src/Test.java
@@ -1,28 +1,42 @@
 import com.atilika.kuromoji.ipadic.Token;
 import com.atilika.kuromoji.ipadic.Tokenizer;
 import java.util.*;
-
+import java.lang.Object;
 
 public class Test {
 	public static void main(String[] args) {
+		HashMap hm = new HashMap();
+		hm.put("タ", "ta");
+		hm.put("ベ", "be");
+		hm.put("ル", "ru");
+		
         Tokenizer tokenizer = new Tokenizer() ;
-        List<Token> tokens = tokenizer.tokenize("お寿司が食べたい。");
+        List<Token> tokens = tokenizer.tokenize("お寿司が食べたい。　桃太郎");
         ArrayList<String> inval = new ArrayList<String>();
         inval.add("記号");
         inval.add("接頭詞");
         inval.add("助詞");
         inval.add("助動詞");
         ArrayList<String> val = new ArrayList<String>();
-        
+        String basewords = "";
         for (Token token : tokens) {
+        	System.out.println(token);
         	boolean valid = inval.contains(token.getPartOfSpeechLevel1());
         	if (!valid) {
         		val.add(token.getBaseForm());
-        		System.out.println(token.getBaseForm() + "\t" + token.getPartOfSpeechLevel1());
+        		System.out.println(token.getBaseForm());
+        		basewords+= token.getBaseForm();
+        
         	}
 
 		}
-        
+        System.out.println(basewords);
+        List<Token> basetokens = tokenizer.tokenize(basewords);
+        for (Token token : basetokens) {
+        	System.out.print(token.getReading());
+        }
+        char kanji = '漢';
+        System.out.println((int)kanji);
 	}
 }
 
