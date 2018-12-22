654d5748604d469eb7461a0cd592128f8c3568bb
Shwuup <e_kong98z@hotmail.com>
2018-12-22 23:09:27 +1100

    add readTexFile method

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,12 @@
-class Main {
+import com.atilika.kuromoji.ipadic.Token
 
+import scala.io.Source
+
+object Main extends App{
+  def readTextFile() = {
+    val readTextFile: Iterator[String] = Source.fromResource("test.txt").getLines
+    for(lines <- readmeText) {
+      println(lines)
+    }
+  }
 }
