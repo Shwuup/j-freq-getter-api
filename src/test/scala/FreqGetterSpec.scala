7fb60d410b647080828dd623cb822cbbecde8edd
Shwuup <e_kong98z@hotmail.com>
2019-01-01 23:49:28 +1100

    update test a tiny bit

--- src/test/scala/FreqGetterSpec.scala
+++ src/test/scala/FreqGetterSpec.scala
@@ -1,3 +1,13 @@
-class FreqGetterSpec {
+import io.github.shwuup.freq.FreqService
+import org.scalatest.{FunSuite, Matchers}
+import cats.effect.{IO, _}
+import org.http4s.{Method, Request, Status, Uri}
+
+
+class FreqGetterSpec extends FunSuite with Matchers  {
+
+  val service = FreqService()
+  val resp = service.run(Request(method = Method.GET, uri = Uri.uri("/hello/bob")))
+
 
 }
