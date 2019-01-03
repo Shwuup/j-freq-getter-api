6db6d6cdad945b84be62a613fef68a5695d97a00
Shwuup <e_kong98z@hotmail.com>
2019-01-04 00:05:58 +1100

    imperative vocab freq and total vocab capability implemented

--- src/main/scala/io/github/shwuup/freq/FreqGetter.scala
+++ src/main/scala/io/github/shwuup/freq/FreqGetter.scala
@@ -1,16 +1,35 @@
 package io.github.shwuup.freq
 import com.atilika.kuromoji.ipadic.Tokenizer
-
+import collection.JavaConverters._
 import scala.collection.mutable
 import scala.io.Source
 
 object FreqGetter {
   def apply(japText: String, jlptDic: mutable.Map[String, String]) = {
     val tokenizer = new Tokenizer()
-    val tokens = tokenizer.tokenize(japText)
-    tokens.forEach(token => println(token.getSurface + "\t"
-      + token.getBaseForm + "\t"
-      + token.getPartOfSpeechLevel1 + "\t"
-      + token.getAllFeatures))
+    val tokens = tokenizer.tokenize(japText).asScala
+    var totalWords = 0
+    var wordsInText = scala.collection.mutable.HashMap.empty[String, (String, Int)]
+
+    for (tok <- tokens) {
+      val word = tok.getBaseForm
+      val level = jlptDic.get(word)
+      level match {
+        case Some(value) =>
+          if (wordsInText.contains(word)) {
+            val newFreq = wordsInText.getOrElse(word, ("not found", 0))._2 + 1
+            wordsInText += (word -> (value, newFreq))
+          }
+          else wordsInText += (word -> (value, 1))
+          totalWords+=1
+        case None => println(s"$word not found")
+      }
+    }
+    (totalWords, wordsInText.toList)
+
+//    tokens.forEach(token => println(token.getSurface + "\t"
+//      + token.getBaseForm + "\t"
+//      + token.getPartOfSpeechLevel1 + "\t"
+//      + token.getAllFeatures))
   }
 }
--- src/test/scala/FreqGetterSpec.scala
+++ src/test/scala/FreqGetterSpec.scala
@@ -1,4 +1,4 @@
-import io.github.shwuup.freq.FreqService
+import io.github.shwuup.freq.{FreqGetter, FreqService, JLPTDic}
 import org.scalatest.{FunSuite, Matchers}
 import cats.effect.{IO, _}
 import org.http4s.{Method, Request, Status, Uri}
@@ -6,8 +6,12 @@ import org.http4s.{Method, Request, Status, Uri}
 
 class FreqGetterSpec extends FunSuite with Matchers  {
 
-  val service = FreqService()
-  val resp = service.run(Request(method = Method.GET, uri = Uri.uri("/hello/bob")))
-
-
+  test("A list of tuples with correct frequencies, vocab and jlpt level and total is returned") {
+    val jlptDic = JLPTDic.apply()
+    val text =  "彼女は日本語を上手に話した。上手"
+    val freq = FreqGetter(text, jlptDic)
+    freq._2.foreach(println(_))
+    freq._1 shouldBe 4
+    freq._2 shouldBe List(("上手",("5",2)), ("話す",("5",1)),("彼女",("4",1)))
+  }
 }
