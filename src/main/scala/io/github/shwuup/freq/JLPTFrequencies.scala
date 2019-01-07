103229c8b7a888d768e45488d9143491fed85854
Shwuup <e_kong98z@hotmail.com>
2019-01-07 13:58:00 +1100

    refactor to correct name

--- src/main/scala/io/github/shwuup/freq/JLPTFrequencies.scala
+++ src/main/scala/io/github/shwuup/freq/JLPTFrequencies.scala
@@ -1,11 +1,11 @@
 package io.github.shwuup.freq
 
 case class JLPTFrequencies(totalFreq: Int,
-                           jpltTotal: List[JWord],
-                           jplt1List: List[JWord],
-                           jplt2List: List[JWord],
-                           jplt3List: List[JWord],
-                           jplt4List: List[JWord],
-                           jplt5List: List[JWord]) {
+                           jlptTotal: List[JWord],
+                           jlpt1List: List[JWord],
+                           jlpt2List: List[JWord],
+                           jlpt3List: List[JWord],
+                           jlpt4List: List[JWord],
+                           jlpt5List: List[JWord]) {
 
 }
