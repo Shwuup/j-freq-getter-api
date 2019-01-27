6497d7e17faca3d62c6a6092b3e25f221e1f1afa
Shwuup <e_kong98z@hotmail.com>
2019-01-27 18:43:11 +1100

    list rendering changes depending on dropdown value

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import './App.scss';
 import axios from 'axios';
+import { Dropdown, DropdownItem } from 'semantic-ui-react'
+import _ from 'lodash';
 
 class App extends Component {
   constructor(props) {
@@ -10,6 +11,9 @@ class App extends Component {
         jlptTotal: []
       },
       selectedFile: "",
+      isFetched: false,
+      currentList: [],
+      currentListName: "",
     }
   }
 
@@ -17,10 +21,18 @@ class App extends Component {
     this.setState({selectedFile: event.target.files[0]})
   }
 
+  dropdownHandler = (event, data) => {
+    const chosenVal = _.get(this.state.api, data.value)
+    this.setState({ currentList: chosenVal, currentListName: data.text}, () => 
+      console.log(
+        event,data
+      ))
+  }
+
   uploadHandler = () => {
     axios.post("http://0.0.0.0:8001/upload", this.state.selectedFile)
       .then(response => {
-        this.setState({ api: response.data }, () => 
+        this.setState({ api: response.data, isFetched: true }, () => 
           console.log(
             this.state.api.jlptTotal
         ));
@@ -28,17 +40,34 @@ class App extends Component {
   }
 
   render() {
-    axios.get("http://0.0.0.0:8001/")
-      .then(function (response) {
-        console.log(response)
-      }
-    )
     return (
       <React.Fragment>
         <h1>J-FREQ-LIST-GETTER</h1>
         <input type="file" onChange={this.fileChangedHandler}/>
         <button onClick={this.uploadHandler}>Upload</button>
-        <WordLister freqList={this.state.api.jlptTotal}/>
+        {this.state.isFetched &&
+          <Dropdown 
+          placeholder='select level' 
+          fluid 
+          selection 
+          onChange={this.dropdownHandler}
+          options={
+            [
+              {text: 'JLPT1', 
+              value: 'jlpt1List'},
+              {text: 'JLPT2', 
+              value: 'jlpt2List'},
+              {text: 'JLPT3', 
+              value: 'jlpt3List'},
+              {text: 'JLPT4', 
+              value: 'jlpt4List'},
+              {text: 'JLPT5', 
+              value: 'jlpt5List'},
+              {text: 'OVERALL', 
+              value: 'jlptTotal'}
+              ]}
+              />}
+        <WordLister freqList={this.state.currentList} name={this.state.currentListName}/>
       </React.Fragment>
     );
   }
@@ -50,7 +79,6 @@ function WordLister(props) {
   const freqListElement = props.freqList.map(w => 
     <li>
       Word: {w.word}<br></br>
-      JLPT: {w.jlptLevel}<br></br>
       Frequency: {w.freq}
     </li>
   )
