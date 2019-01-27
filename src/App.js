f777335cef87db74a7bc308c081cf20560ca519f
Shwuup <e_kong98z@hotmail.com>
2019-01-27 15:20:37 +1100

    render api call

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      api: "",
+      api: {
+        jlptTotal: []
+      },
+      test: "",
       selectedFile: "",
     }
   }
@@ -17,26 +20,40 @@ class App extends Component {
 
   uploadHandler = () => {
     axios.post("http://0.0.0.0:8001/upload", this.state.selectedFile)
-      .then(function (response) {
-        this.setState({api: response})
-      })
+      .then(response => {
+        this.setState({ api: response.data }, () => 
+          console.log(
+            this.state.api.jlptTotal
+        ));
+      });
   }
 
   render() {
-    const apiCall = 
+    let jlpt = this.state.api.jlptTotal
     axios.get("http://0.0.0.0:8001/")
       .then(function (response) {
         console.log(response)
       }
     )
-
     return (
       <React.Fragment>
         <input type="file" onChange={this.fileChangedHandler}/>
         <button onClick={this.uploadHandler}>Upload</button>
+        <WordLister freqList={this.state.api.jlptTotal}/>
       </React.Fragment>
     );
   }
 }
 
 export default App;
+
+function WordLister(props) {
+  return (
+    <div>
+      {props.freqList.map((w, index) => 
+      <h1>
+        {index + 1}. Word: {w.word} <br/> JLPT Level: {w.jlptLevel} <br/> Frequency: {w.freq}
+      </h1>)}
+    </div>
+  );
+}
