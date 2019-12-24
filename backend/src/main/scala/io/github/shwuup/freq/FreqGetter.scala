c6d4dde4b7d33907050637fe8d78cc7bcf683792
Shwuup <e_kong98z@hotmail.com>
2019-12-24 13:27:48 +1100

    refactor apply function in FreqGetter

--- backend/src/main/scala/io/github/shwuup/freq/FreqGetter.scala
+++ backend/src/main/scala/io/github/shwuup/freq/FreqGetter.scala
@@ -1,9 +1,7 @@
 package io.github.shwuup.freq
-import com.atilika.kuromoji.ipadic.Tokenizer
+import com.atilika.kuromoji.ipadic.{Token, Tokenizer}
 import com.typesafe.scalalogging.LazyLogging
-
 import collection.JavaConverters._
-import scala.collection.mutable
 import scala.io.Source
 
 object FreqGetter extends LazyLogging {
@@ -17,33 +15,31 @@ object FreqGetter extends LazyLogging {
     jlptDic
   }
 
-  def apply(japText: String): JLPTFrequencies = {
+  def apply(text: String): JLPTFrequencies = {
     val tokenizer = new Tokenizer()
-    val tokens = tokenizer.tokenize(japText).asScala
-    var wordsInText = Map.empty[String, JWord]
-    for (tok <- tokens) {
-      val word = tok.getBaseForm
-      val level = jlptDic.get(word)
-      level match {
-        case Some(value) =>
-          if (wordsInText.contains(word)) {
-            val oldJWord = wordsInText.getOrElse(word, throw new Exception("hey this shouldn't happen"))
-            val newJWord = oldJWord.copy(freq = oldJWord.freq + 1)
-            wordsInText += (word -> newJWord)
-          }
-          else if (!wordsInText.contains(word)) {
-            wordsInText += (word -> JWord(word,value, 1))
+    val tokens:List[Token] = tokenizer.tokenize(text).asScala.toList
+    val tokensInJlptDic = tokens.filter(t => jlptDic.contains(t.getBaseForm))
+    val wordsWithFrequencies:List[(String, String)] = tokensInJlptDic.map(t => (t.getBaseForm, jlptDic.getOrElse(t.getBaseForm, throw new Exception("not found"))))
+    getJLPTFrequencies(populateFrequencyMap(wordsWithFrequencies))
+    }
 
-          }
-        case None =>
+  def populateFrequencyMap(frequencies: List[(String, String)]):Map[String,JWord] = {
+    val wordsInText = Map.empty[String, JWord]
+    frequencies.foldLeft(wordsInText)((acc, w) => {
+      if (acc.contains(w._1)) {
+        val oldWord = acc.getOrElse(w._1, throw new Exception("word not found"))
+        val newWord = oldWord.copy(freq = oldWord.freq + 1)
+        acc + (w._1 -> newWord)
+      } else {
+        acc + (w._1 -> JWord(w._1,w._2, 1))
       }
-    }
-    val getJLPTFrequencies: JLPTFrequencies = {
-      val jlptTotalFreqList = wordsInText.toList.map(x => x._2).sorted
-      val totalWords = jlptTotalFreqList.foldLeft(0)((x, y) => x + y.freq)
-      val listOfFreq = for (i <- 1 to 5) yield jlptTotalFreqList.foldRight(List[JWord]())((x, y) => if (x.jlptLevel == s"$i") x :: y else y).take(10)
-      JLPTFrequencies(totalWords, jlptTotalFreqList.take(10), listOfFreq(0), listOfFreq(1), listOfFreq(2), listOfFreq(3), listOfFreq(4))
-    }
-    getJLPTFrequencies
+    })
+  }
+
+  def getJLPTFrequencies(frequencies:Map[String,JWord]): JLPTFrequencies = {
+    val jlptTotalFreqList = frequencies.toList.map(x => x._2).sorted
+    val wordTotal = jlptTotalFreqList.foldLeft(0)((x, y) => x + y.freq)
+    val listOfFreq = for (i <- 1 to 5) yield jlptTotalFreqList.foldRight(List[JWord]())((x, y) => if (x.jlptLevel == s"$i") x :: y else y).take(10)
+    JLPTFrequencies(wordTotal, jlptTotalFreqList.take(10), listOfFreq(0), listOfFreq(1), listOfFreq(2), listOfFreq(3), listOfFreq(4))
   }
 }
